8126000216794ab203df031c86191cc6f0493627
Amandeep Singh <programmeramandeep@gmail.com>
2021-01-13 09:21:18 +0530

    setup auth files

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", [LandingPageController::class, 'index'])->name('landing.page');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
